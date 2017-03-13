8e83c9a190ee3dc70a79739b582f89ff3214c984
Alinkaa <jankon@mail.ru>
2017-03-13 12:08:44 +0200

    get_short_text medhod in model

--- gis/models.py
+++ gis/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
+SHORT_EXT_LEN = 700
 
 # Create your models here.
 class Articles(models.Model):
@@ -12,3 +13,9 @@ class Articles(models.Model):
 
     def __str__(self):
         return(self.title)
+
+    def get_short_text(self):
+        if len(self.text) > SHORT_EXT_LEN:
+            return self.text[:SHORT_EXT_LEN]
+        else:
+            return self.text
